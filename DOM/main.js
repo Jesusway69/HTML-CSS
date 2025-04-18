1450189255d063a2de9141433a85476393f14bb7
JesusWay69 HP elitebook <jesusway69@gmail.com>
2025-04-18 23:46:55 +0200

    20250418 Ejercicios libres con DOM, terminado ejercicio input autoindexado

--- DOM/main.js
+++ DOM/main.js
@@ -1,18 +1,32 @@
-let pageTitle = document.getElementById("pageTitle")
-let itemInput = document.getElementById("itemInput")
-let addButton = document.querySelector(".addButton")
-let dynamicList = document.querySelector(".dynamicList")
+const pageTitle = document.getElementById("pageTitle")
+const itemInput = document.getElementById("itemInput")
+const addButton = document.querySelector(".addButton")
+const dynamicList = document.querySelector(".dynamicList")
+const filterInput = document.getElementById("filterInput")
+const filterButton = document.getElementsByClassName("filterButton")
+const list = document.getElementsByTagName("ul")
 
 
-
-addButton.addEventListener("click", function () {
-    if (itemInput.value !== ""){
+addButton.addEventListener("click", () => {
+    if (itemInput.value !== "") {
         let newElement = document.createElement("li")
         newElement.innerText = itemInput.value
         pageTitle.innerText = "Aprendiendo el DOM"
         dynamicList.appendChild(newElement)
         itemInput.value = ""
     }
-    
+
 })
 
+filterInput.addEventListener("keyup", () => {
+    const indexFind = filterInput.value.toLowerCase()
+    const items = document.getElementsByTagName("li")
+
+    Array.from(items).forEach(function (item) {
+        if (item.textContent.toLowerCase().indexOf(indexFind) !== -1) {
+            item.style.display = "block"
+        } else {
+            item.style.display = "none"
+        }
+    })
+})
